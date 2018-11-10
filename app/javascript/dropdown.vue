f0933c05d215e8e4ee4690d0a38606b1006d64ac
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-10 21:03:40 +0200

    javascript packs

---
+++ app/javascript/dropdown.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="dropdown">
+    <a class="nav-avatar" @click="toggleActive">
+      <!-- <%= image_tag gravatar_url(current_user.email, :size=> 51), alt: "nav-img", class: "nav-img" %> -->
+    </a>
+    <!-- menu -->
+    <div class="menu-container" v-bind:class="{ active: active }">
+      <ul class="user-menu">
+
+        <li><!-- <%= link_to user_path(current_user), class: 'user-menu-link' do %> -->
+          <i class="fa fa-user"></i> Profile
+        </li>
+        
+        <li><!-- <%= link_to admin_root_path, class: 'user-menu-link' do %> -->
+          <i class="fa fa-lock"></i> Admin
+        </li>
+        
+        
+
+        <div class="divider"></div>
+
+        <li><!-- <%= link_to destroy_user_session_path, class: 'user-menu-link', :method => :delete  do %> -->
+          <i class="fa fa-power-off"></i> Log Out
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        active: false,
+        email: null
+      }
+    },
+    methods: {
+        toggleCurrent (event) {
+          event.currentTarget.classList.toggle('active');
+        },
+        toggleActive() {
+          this.active = !this.active;
+        }
+    }
+  }
+</script>
+
+<style>
+</style>
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,22 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import Dropdown from '../dropdown.vue'
+import Rails from 'rails-ujs'
+
+Rails.start()
+
+Vue.use(TurbolinksAdapter)
+
+Vue.component('dropdown', Dropdown)
+
+document.addEventListener('DOMContentLoaded', () => {
+   // Setup headers of X-CSRF-Token that it grabs from rails generated token in meta tag.
+  // axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+
+  const app = new Vue({
+    el: '[data-action="vue"]',
+  })
+})
