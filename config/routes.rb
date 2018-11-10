67c35515b36c391984da8317a9e3b35b0b84f9a4
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-10 21:08:59 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+      resources :posts
+      resources :users
+      resources :comments
+
+      root to: 'posts#index'
+    end
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq' 
+
   devise_for :users
-  root to: "landing#index"
+  root 'landing#index'
+
+  resources :posts do
+    resources :comments
+    member do
+      put 'like', to:    'posts#upvote'
+      put 'dislike', to: 'posts#downvote'
+    end
+  end
+
+  resources :users
+  resources :quizzes
+  resources :comments, only: :index
+
 end
