4670b53d1ad6c1ba258e2e8f11f6932b453665b0
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-10 20:52:49 +0200

    generate posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,101 @@
+class PostsController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  before_action :sanitize_content, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.order(:cached_votes_score => :desc).page(params[:page]).per(5)
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = current_user.posts.build
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_back(fallback_location: posts_path)
+  end
+
+  def downvote
+    @post.downvote_from current_user
+    redirect_back(fallback_location: posts_path)
+  end
+
+  private
+
+  def require_same_user
+    if current_user != @post.user
+      flash[:danger] = "Not authorized to edit this post"
+      redirect_to root_path
+    end
+  end
+
+    # Sanitize user generated HTML and CSS
+    def sanitize_content
+      Sanitize.fragment(@post, Sanitize::Config::RELAXED)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.friendly.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  acts_as_votable
+  belongs_to :user
+  has_many :comments
+end
