f3ecedab8b45561029b666806de049da540433a3
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-10 21:09:37 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  extend FriendlyId
+    friendly_id :username, use: :slugged
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  has_many :posts
+  has_many :comments
+
+  # ensure usernames are unique
+  # validates_uniqueness_of :username
+
+  acts_as_voter
 end
