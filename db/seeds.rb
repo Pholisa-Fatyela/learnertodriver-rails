1e4b38564ec8c101c2707dd6f4c6b9710917b810
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-12 09:47:37 +0200

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+include Sprig::Helpers
+User.delete_all
+Quiz.delete_all
+Post.delete_all
+Comment.delete_all
+sprig [ User, Quiz, Post, Comment ]
