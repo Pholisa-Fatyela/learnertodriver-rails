b188ed93dc6f5bea8f1fa53e550bf44090a8ec44
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-12 22:59:47 +0200

    comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    @comments = Comment.all
+  end
+
+  def edit
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: posts_path, notice: 'Comment successfully deleted') }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:post_id, :content, :user_id)
+    end
+end
