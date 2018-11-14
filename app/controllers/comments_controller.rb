c6b7bbc317a9170e39659444eac7303dc5a59c54
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-11-14 17:32:26 +0200

    comment content strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,10 +18,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def index
-    @comments = Comment.all
-  end
-
   def edit
   end
 
@@ -43,6 +39,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:post_id, :content, :user_id)
+      params.require(:comment).permit(:post_id, :body, :user_id)
     end
 end
