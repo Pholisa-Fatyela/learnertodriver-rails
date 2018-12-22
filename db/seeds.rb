8aa0538cd5d65ff357863e6d9b24235983e8cb2b
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2018-12-22 21:37:16 +0200

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 include Sprig::Helpers
 
+puts "seeds.rb"
+
 User.delete_all
 Quiz.delete_all
 Post.delete_all
 Comment.delete_all
 Question.delete_all
 
+puts "cleaned data User, Quiz, Post, Comment, Question"
+
 sprig [ User, Quiz, Post, Comment, Question ]
+
+puts "new data seeded"
