ef1a32cb6aad729ad1f35a79546cca34065459fc
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2019-04-22 10:48:28 +0200

    remove vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,22 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import TurbolinksAdapter from 'vue-turbolinks'
-import Vue from 'vue/dist/vue.esm'
 import Rails from 'rails-ujs'
-import Dropdown from '../dropdown.vue'
 
 Rails.start()
-
-Vue.use(TurbolinksAdapter)
-
-Vue.component('dropdown', Dropdown)
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Setup headers of X-CSRF-Token that it grabs from rails generated token in meta tag.
-  // axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-
-  const app = new Vue({
-    el: '[data-action="vue"]',
-  })
-})
