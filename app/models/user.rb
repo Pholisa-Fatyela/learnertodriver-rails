6c3631dbab5789d9794b7d0c78df521be803fee6
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2019-04-23 23:46:41 +0200

    parameterize username slug before save, for pretty urls

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save :parameterize_slug
+
   extend FriendlyId
-    friendly_id :username, use: :slugged
+    friendly_id :name, use: :slugged
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -10,7 +12,12 @@ class User < ApplicationRecord
   has_many :comments
 
   # ensure usernames are unique
-  # validates_uniqueness_of :username
+  validates_uniqueness_of :username
 
   acts_as_voter
+
+  private
+    def parameterize_slug
+      self.slug.parameterize
+    end
 end
