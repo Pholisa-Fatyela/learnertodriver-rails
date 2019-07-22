d1fd653ada568d3b6bb362bfa8240bbbe8b80522
siyanda <maphumulo.ps@gmail.com>
2019-07-22 02:09:15 +0200

    Q&A with self ref for A

--- db/migrate/20181112202516_create_questions.rb
+++ db/migrate/20181112202516_create_questions.rb
@@ -2,12 +2,8 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.string :content
-      t.string :option1
-      t.string :option2
-      t.string :option3
-      t.string :image_url
       t.string :explanation
-      t.string :answer
+      t.references :quiz, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20181205163413_add_quiz_id_to_questions.rb
+++
@@ -1,4 +0,0 @@
-class AddQuizIdToQuestions < ActiveRecord::Migration[5.2]
-    add_column :questions, :quiz_id, :integer
-    add_index :questions, :quiz_id
-end
