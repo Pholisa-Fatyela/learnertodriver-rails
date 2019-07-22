515e9aa35a8c524f5cbc48b05f395bc558310641
siyanda <maphumulo.ps@gmail.com>
2019-07-22 02:11:55 +0200

    Rails 6 upgrade, active_text, active_storage

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,60 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_163413) do
+ActiveRecord::Schema.define(version: 2019_07_21_233143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.string "explanation"
+    t.bigint "question_id"
+    t.bigint "correct_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["correct_id"], name: "index_answers_on_correct_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -57,15 +99,10 @@ ActiveRecord::Schema.define(version: 2018_12_05_163413) do
 
   create_table "questions", force: :cascade do |t|
     t.string "content"
-    t.string "option1"
-    t.string "option2"
-    t.string "option3"
-    t.string "image_url"
     t.string "explanation"
-    t.string "answer"
+    t.bigint "quiz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quiz_id"
     t.index ["quiz_id"], name: "index_questions_on_quiz_id"
   end
 
@@ -124,6 +161,10 @@ ActiveRecord::Schema.define(version: 2018_12_05_163413) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "answers", column: "correct_id"
+  add_foreign_key "answers", "questions"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "questions", "quizzes"
 end
