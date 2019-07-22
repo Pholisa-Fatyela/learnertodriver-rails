0850dfd7c832fa0980c187cb1a9a4c182d7831f4
siyanda <maphumulo.ps@gmail.com>
2019-07-22 02:09:57 +0200

    remove srig from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,6 @@ include Sprig::Helpers
 
 puts "seeds.rb"
 
-Quiz.delete_all
-Question.delete_all
-Comment.delete_all
-Post.delete_all
-User.delete_all
-
 puts "cleaned data User, Quiz, Post, Comment, Question"
-
-sprig [ User, Quiz, Post, Comment, Question ]
 
 puts "new data seeded"
