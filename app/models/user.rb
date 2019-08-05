e18685e5181f356708fa00269e3a4e89174871e8
siyanda <maphumulo.ps@gmail.com>
2019-08-05 07:48:04 +0200

    update user model adds service worker controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  before_save :parameterize_slug
+  before_save :create_name, :suggest_username
 
   extend FriendlyId
-    friendly_id :name, use: :slugged
+    friendly_id :username, use: :slugged
+
+  # ensure usernames are unique
+  validates_uniqueness_of :username
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -11,13 +14,22 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  # ensure usernames are unique
-  validates_uniqueness_of :username
-
   acts_as_voter
 
+  def should_generate_new_friendly_id?
+    slug.nil?  || username_changed?
+  end
+
   private
-    def parameterize_slug
-      self.slug.parameterize
+    def create_name
+      self.first_name = "none" if first_name.blank?
+      self.last_name = "none" if last_name.blank?
+      self.name = "#{first_name} #{last_name}"
+    end
+
+    def suggest_username
+      self.username = name.parameterize
+      # if self.username
+      # self.username = name.parameterize
     end
 end
