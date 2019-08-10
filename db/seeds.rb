f991e6ea07d9bb1e5e02af797b3186b6235e12a3
siyanda <maphumulo.ps@gmail.com>
2019-08-10 03:46:57 +0200

    update db, faker seeds deprecated items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 models =  %w( answer question quiz comment post user)
 
-puts "... seeds.rb"
-
-Comment.delete_all
-Post.delete_all
-User.delete_all
-Response.delete_all
-Answer.delete_all
-Question.delete_all
-Quiz.delete_all
-
-puts "... cleaned data User, Quiz, Post, Comment"
-
-sprig [ User, Post, Comment, Quiz, Question, Answer, Response ]
->>>>>>> master
+puts "... deleting all existing data 🗑"
+  
+  models.each { |m|
+    m.camelize.constantize.delete_all
+  }
+  
+  # seed_modules
+  
+  puts "... seeding new data 💾"
+  models.reverse.each do |data|
+    require File.expand_path(File.dirname(__FILE__))+"/seeds/modules/#{ data }.rb"
+  end
 
 models.each { |m|
   puts "#{ m.camelize.constantize.count } #{ m.pluralize } created"
