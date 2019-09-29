253ce1acc470e0312feae1b7e90def316f3c7c60
siyanda <maphumulo.ps@gmail.com>
2019-09-29 13:02:35 +0200

    corrects Q & A

--- db/migrate/20181112501522_create_answers.rb
+++ db/migrate/20181112501522_create_answers.rb
@@ -3,7 +3,6 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
     create_table :answers do |t|
       t.string :content
       t.string :explanation
-      t.references :question, foreign_key: true
       t.references :correct, foreign_key: { to_table: :answers }, index: true
 
       t.timestamps
