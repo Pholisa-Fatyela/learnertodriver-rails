1d1932df93d40e04ef7b15853afbc030500befc4
siyanda <maphumulo.ps@gmail.com>
2019-09-29 13:05:54 +0200

    updates quiz view

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -14,7 +14,7 @@ class QuizzesController < ApplicationController
   # GET /quizzes/1
   # GET /quizzes/1.json
   def show
-    @questions = Question.page(params[:page]).per(1)
+    @questions = @quiz.questions.order(:id => :asc).includes(:answers)
   end
 
   # GET /quizzes/new
